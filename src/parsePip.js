cfa112fd8d9a5fc39e2d18deea454e50ca08bdc8
Erik <eko24old@gmail.com>
2018-04-21 13:00:57 +0300

    Fixed pip parsing

--- src/parsePip.js
+++ src/parsePip.js
@@ -7,12 +7,12 @@ const parsePip = msg => {
         const charismaAgilityRegExp = /🗣Харизма: ([\d]*).🤸🏽‍♂️Ловкость: ([\d]*)/g;
         const damageArmorRegExp = /⚔️Урон: ([\d]*) 🛡Броня: ([\d]*)/g;
         const strengthDexterityRegExp = /💪Сила: ([\d]*) 🔫Меткость: ([\d]*)/g;
-        const enduranceRegExp = /🔋Выносливость: ([\d]+)\/([\d]+)/g;
+        const enduranceRegExp = /🔋Выносливость: (\d*)\/(\d*)/g;
         const hungerRegExp = /🍗Голод: ([\d]*)%/g;
         const healthRegExp = /❤️Здоровье: (\d*)\/(\d*)/g;
 
         const [, parsedCharisma, parsedAgility] = charismaAgilityRegExp.exec(msg);
-        const [, parsedDamage, parsedArmor] = damageArmorRegExp.exec(msg);
+        // const [, parsedDamage, parsedArmor] = damageArmorRegExp.exec(msg);
         const [, parsedStrength, parsedDexterity] = strengthDexterityRegExp.exec(msg);
         const [, enduranceNow, enduranceMax] = enduranceRegExp.exec(msg);
         const [, parsedHunger] = hungerRegExp.exec(msg);
@@ -26,8 +26,6 @@ const parsePip = msg => {
             healthNow,
             healthMax,
             parsedHunger,
-            parsedDamage,
-            parsedArmor,
             parsedStrength,
             parsedDexterity,
             parsedCharisma,
@@ -38,6 +36,7 @@ const parsePip = msg => {
             }
         };
     } catch (e) {
+        console.log(e);
         try {
             msg = msg.split("\n").join("");
 
@@ -71,6 +70,7 @@ const parsePip = msg => {
                 }
             };
         } catch (e) {
+
             return false;
         }
     }
