101902895e11665ac968958019ca22adba015b8f
Erik <eko24old@gmail.com>
2018-05-07 06:41:44 +0300

    added location scheme

---
+++ src/schemes/location.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const locationScheme = new Schema({
+    distance: Number,
+    name: String,
+    type: String,
+    effects: [String],
+    capsReceived: [Number],
+    materialsReceived: [Number],
+    capsLost: [Number],
+    materialsLost: [Number],
+    receivedItems: [Schema.Types.Mixed],
+    healthInjuries: [Number],
+    lastUpdated: String
+});
