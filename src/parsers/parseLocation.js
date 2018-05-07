18ff40262d8c88bc3f322fb786ce0a8eae572ce6
Erik <eko24old@gmail.com>
2018-05-07 09:01:33 +0300

    location parse [WIP]

---
+++ src/parsers/parseLocation.js
@@ -0,0 +1,21 @@
+const {
+    regexps
+} = require('../regexp/regexp');
+
+const parseLocation = location => {
+    const splitted = location.split('\n');
+    
+    const distance = ;
+    const [, name] = locationNameRegExp.exec(location);
+    const type = 'unknown';
+    const effects = ;
+    const capsReceived = ;
+    const materialsReceived = ;
+    const capsLost = ;
+    const materialsLost = ;
+    const receivedItems = ;
+    const healthInjuries = ;
+    const lastUpdated = ;
+};
+
+module.exports = parseLocation;
