30b6b0f33ddc646927f74be1e26cf4e0bde43a15
Erik <eko24old@gmail.com>
2018-05-08 13:29:48 +0300

    added RegExps

--- src/regexp/regexp.js
+++ src/regexp/regexp.js
@@ -21,6 +21,7 @@ const beastStunRegExp = /(.+) оглушен ударом 💫/;
 const playerBeastAttackRegExp = /👤Ты .+ 💥(\d+)/;
 const dungeonBeastAppeared = /(.+) перегородил тебе путь/;
 const beastDefeatRegExp = /Тебя буквально размазали/;
+const beastDefeatFleeRegExp = /Ты проиграл в этой схватке/;
 const beastVictoryRegExp = /Ты одержал победу!/;
 const beastDefeatCapsLostRegExp = /Потеряно крышек: 🕳(\d+)/;
 const beastDefeatMaterialsLostRegExp = /Потеряно материалов: 📦(\d+)/;
@@ -28,6 +29,18 @@ const deathMessageRegExp = /Потеряно: 🕳(\d+) и 📦(\d+)/;
 
 const junkmanRegExp = /Ты встретил бродячего торговца/;
 const junkManItems = /(.+) — (\d+)/g;
+
+const deathMessageHeaderRegExp = /Эта вылазка могла бы стать последней для тебя/;
+const deathMessageContentRegExp = /Спустя какое-то время ты пришел в себя в своем лагере/;
+const deathMessageRecourcesLostRexExp = /Потеряно: 🕳(\d+) и 📦(\d+)/;
+
+const beastFacedRegExp = /Во время вылазки на тебя напал (.+)\./;
+const dungeonBeastFacedRegExp = /(.+) перегородил тебе путь./;
+
+const metalAmountRegExp = /(.+) (\d+)/; // 🔗Кубонит 192
+const multipleItemsReceived = /(.+) x(\d+)/; //Провода x1
+const emojiRecourceAmount = /(.+) [\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}](\d+)/u; //Кварц 🔹1
+const bonusEmojiResourceAmount = /([\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}])\+(\d+)/u; //Бонус: 🔩+4
 
 const every = {
     contains: [healthRegExp, hungerRegExp, staminaRegExp, campDistanceRegExp]
@@ -45,7 +58,8 @@ const location = {
         materialsLostRegExp,
         receivedItemRegExp,
         actionReceivedCapsRegExp,
-        actionReceivedMaterialsRegExp
+        actionReceivedMaterialsRegExp,
+        beastFacedRegExp
     ],
     excludes: [beastNameRegExp]
 };
@@ -84,6 +98,30 @@ const dungeonBeast = {
     ]
 }
 
+const fleeDefeat = {
+    contains: [
+        every.contains,
+        injuryRegExp,
+        beastDefeatFleeRegExp,
+        beastDefeatCapsLostRegExp,
+        beastDefeatMaterialsLostRegExp
+    ]
+}
+
+const deathMessage = {
+    contains: [
+        deathMessageHeaderRegExp,
+        deathMessageContentRegExp,
+        deathMessageRecourcesLostRexExp
+    ]
+};
+
+const dungeonBeastFaced = {
+    contains: [
+        dungeonBeastFacedRegExp
+    ]
+};
+
 const regexps = {
     locationNameRegExp,
     locationRaidPostfixRegExp,
@@ -111,7 +149,13 @@ const regexps = {
     beastDefeatMaterialsLostRegExp,
     deathMessageRegExp,
     junkmanRegExp,
-    junkManItems
+    junkManItems,
+    deathMessageRecourcesLostRexExp,
+    beastFacedRegExp,
+    metalAmountRegExp,
+    emojiRecourceAmount,
+    bonusEmojiResourceAmount,
+    multipleItemsReceived
 }
 
 module.exports = {
@@ -119,5 +163,8 @@ module.exports = {
     location,
     regularBeast,
     dungeonBeast,
+    fleeDefeat,
+    deathMessage,
+    dungeonBeastFaced,
     regexps
 };
