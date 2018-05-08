cfc4d2bbafa0d187da116dafee27e90f99137396
Erik-Kotsiuba <Erik.Kotsiuba@onseo.biz>
2018-05-08 18:00:00 +0300

    updated test data; added items normalize; seeding of locations

--- test.js
+++ test.js
@@ -1,40 +1,9 @@
+const _ = require('underscore');
+const moment = require('moment');
+
 const {
     regexps
-} = require('../regexp/regexp');
-
-const data = {
-    "name": "Палаточный лагерь",
-    "distance": "47",
-    "type": "unknown",
-    "capsReceived": 290,
-    "materialsReceived": 296,
-    "capsLost": null,
-    "materialsLost": null,
-    "healthInjuries": null,
-    "receivedItems": ["🔗Кубонит 192", "Сухари", "Микрочип 💾1 ", "Иридий 🔩1"],
-    "receivedBonusItems": ["Провода x1"],
-    "beastFaced": {
-        "faced": false,
-        "name": null
-    },
-    "effect": "good"
-};
-
-data.receivedItems.forEach(item => {
-    if(regexps.locationRaidPostfixRegExp.test(name)) {
-        name = name.replace(regexps.locationRaidPostfixRegExp);
-        isRaid = true;
-    }
-    metalAmountRegExp,
-    emojiRecourceAmount,
-    bonusEmojiResourceAmount,
-    multipleItemsReceived
-});
-
-
-
-/* const _ = require('underscore');
-const moment = require('moment');
+} = require('./src/regexp/regexp');
 
 const data = [{
     "data": {
@@ -56,19 +25,21 @@ const data = [{
 }, {
     "data": {
         "name": "Мост",
+        "isRaid": false,
         "distance": "45",
         "type": "unknown",
-        "capsReceived": null,
-        "materialsReceived": null,
-        "capsLost": null,
-        "materialsLost": null,
-        "healthInjuries": null,
+        "capsReceived": 0,
+        "materialsReceived": 0,
+        "capsLost": 0,
+        "materialsLost": 0,
+        "healthInjuries": 0,
         "receivedItems": [],
         "receivedBonusItems": [],
         "beastFaced": {
             "faced": true,
             "name": "🐉Туннельщик (🏵🏵🏵Мифический)"
-        }
+        },
+        "effect": "none"
     },
     "dataType": "location",
     "date": 1525701951
@@ -79,7 +50,7 @@ const data = [{
         "isDungeon": false,
         "capsReceived": "2516",
         "materialsReceived": "3490",
-        "receivedItems": "🔗Кубонит х155",
+        "receivedItems": ["🔗Кубонит х155"],
         "damageReceived": ["61"],
         "damagesGiven": ["459", "341"],
         "fightResult": "win",
@@ -90,13 +61,14 @@ const data = [{
 }, {
     "data": {
         "name": "Палаточный лагерь",
+        "isRaid": false,
         "distance": "47",
         "type": "unknown",
         "capsReceived": 290,
         "materialsReceived": 296,
-        "capsLost": null,
-        "materialsLost": null,
-        "healthInjuries": null,
+        "capsLost": 0,
+        "materialsLost": 0,
+        "healthInjuries": 0,
         "receivedItems": ["🔗Кубонит 192", "Сухари"],
         "receivedBonusItems": ["Провода x1"],
         "beastFaced": {
@@ -110,125 +82,21 @@ const data = [{
 }, {
     "data": {
         "name": "Заброшенный супермаркет",
+        "isRaid": false,
         "distance": "49",
         "type": "unknown",
-        "capsReceived": null,
-        "materialsReceived": null,
-        "capsLost": null,
-        "materialsLost": null,
-        "healthInjuries": null,
+        "capsReceived": 0,
+        "materialsReceived": 0,
+        "capsLost": 0,
+        "materialsLost": 0,
+        "healthInjuries": 0,
         "receivedItems": [],
         "receivedBonusItems": [],
         "beastFaced": {
             "faced": true,
             "name": "🦅Касадор (🔱Особь 60)"
-        }
-    },
-    "dataType": "location",
-    "date": 1525702576
-}, {
-    "data": {
-        "distance": "49",
-        "capsLost": "18",
-        "materialsLost": "21",
-        "healthInjuries": "129"
-    },
-    "dataType": "fleeDefeat",
-    "date": 1525702607
-}, {
-    "data": {
-        "capsLost": "10524",
-        "materialsLost": "41258"
-    },
-    "dataType": "deathMessage",
-    "date": 1525702607
-}, {
-    "data": {
-        "version": 2,
-        "faction": "⚙️Убежище 4",
-        "health": 204,
-        "name": "Anatoliy",
-        "damage": 386,
-        "armor": 101,
-        "hunger": 40,
-        "strength": 170,
-        "precision": 15,
-        "charisma": 85,
-        "agility": 58,
-        "endurance": 9
-    },
-    "dataType": "pipboy",
-    "date": 1525700743
-}, {
-    "data": {
-        "name": "Мост",
-        "distance": "45",
-        "type": "unknown",
-        "capsReceived": null,
-        "materialsReceived": null,
-        "capsLost": null,
-        "materialsLost": null,
-        "healthInjuries": null,
-        "receivedItems": [],
-        "receivedBonusItems": [],
-        "beastFaced": {
-            "faced": true,
-            "name": "🐉Туннельщик (🏵🏵🏵Мифический)"
-        }
-    },
-    "dataType": "location",
-    "date": 1525701951
-}, {
-    "data": {
-        "distance": "45",
-        "name": "🐉Туннельщик (🏵🏵🏵Мифический)",
-        "isDungeon": false,
-        "capsReceived": "2516",
-        "materialsReceived": "3490",
-        "receivedItems": "🔗Кубонит х155",
-        "damageReceived": ["61"],
-        "damagesGiven": ["459", "341"],
-        "fightResult": "win",
-        "amountOfConcussions": []
-    },
-    "dataType": "regularBeast",
-    "date": 1525701981
-}, {
-    "data": {
-        "name": "Палаточный лагерь",
-        "distance": "47",
-        "type": "unknown",
-        "capsReceived": 290,
-        "materialsReceived": 296,
-        "capsLost": null,
-        "materialsLost": null,
-        "healthInjuries": null,
-        "receivedItems": ["🔗Кубонит 192", "Сухари"],
-        "receivedBonusItems": ["Провода x1"],
-        "beastFaced": {
-            "faced": false,
-            "name": null
         },
-        "effect": "good"
-    },
-    "dataType": "location",
-    "date": 1525702251
-}, {
-    "data": {
-        "name": "Заброшенный супермаркет",
-        "distance": "49",
-        "type": "unknown",
-        "capsReceived": null,
-        "materialsReceived": null,
-        "capsLost": null,
-        "materialsLost": null,
-        "healthInjuries": null,
-        "receivedItems": [],
-        "receivedBonusItems": [],
-        "beastFaced": {
-            "faced": true,
-            "name": "🦅Касадор (🔱Особь 60)"
-        }
+        "effect": "none"
     },
     "dataType": "location",
     "date": 1525702576
@@ -251,6 +119,46 @@ const data = [{
 }];
 
 
+const normalizeItems = items => {
+    const normalizedItems = {};
+
+    items.forEach(item => {
+        let name = item,
+            amount;
+        if (regexps.metalAmountRegExp.test(item)) {
+            [, name, amount] = regexps.metalAmountRegExp.exec(item);
+        }
+
+        if (regexps.emojiRecourceAmount.test(item)) {
+            [, name, amount] = regexps.emojiRecourceAmount.exec(item);
+        }
+
+        if (regexps.bonusEmojiResourceAmount.test(item)) {
+            [, name, amount] = regexps.bonusEmojiResourceAmount.exec(item);
+        }
+
+        if (regexps.multipleItemsReceived.test(item)) {
+            [, name, amount] = regexps.multipleItemsReceived.exec(item);
+        }
+
+        if (normalizedItems[name]) {
+            const existingAmount = normalizedItems[name];
+
+            if (amount === undefined) {
+                normalizedItems[name] += 1;
+
+            } else {
+                normalizedItems[name] += Number(amount);
+
+            }
+        } else {
+            normalizedItems[name] = Number(amount) || 1;
+        }
+    });
+
+    return normalizedItems;
+};
+
 const reportData = {
     capsReceived: 0,
     capsLost: 0,
@@ -267,7 +175,8 @@ const reportData = {
 };
 
 const updatesData = {
-
+    locations: [],
+    beasts: []
 }
 
 data.sort((first, second) => {
@@ -284,6 +193,8 @@ data.sort((first, second) => {
 }) => {
     switch (dataType) {
         case 'location':
+            const locationData = _.clone(data);
+
             if (data.effect) {
                 if (data.effect === 'bad') {
                     reportData.capsLost -= data.capsLost;
@@ -292,6 +203,9 @@ data.sort((first, second) => {
                     reportData.receivedItems.push(_.flatten([data.receivedItems, data.receivedBonusItems]));
                     reportData.capsReceived += data.capsReceived;
                     reportData.materialsReceived += data.materialsReceived;
+
+                    locationData.receivedItems = normalizeItems(data.receivedItems);
+                    locationData.receivedBonusItems = normalizeItems(data.receivedBonusItems);
                 }
             }
 
@@ -300,6 +214,9 @@ data.sort((first, second) => {
             }
 
             reportData.distance = data.distance;
+
+            delete locationData.beastFaced;
+            updatesData.locations.push(locationData);
 
             break;
         case 'regularBeast':
@@ -317,9 +234,9 @@ data.sort((first, second) => {
         case 'fleeDefeat':
             reportData.lastFleeDefeat = data.distance;
 
-            if (reportData.lastPip) {
-                updatesData.flees = `Unsucsessfull flee from ${reportData.lastBeastSeen.name} on ${reportData.distance} with agility ${reportData.lastPip.agility}`;
-            }
+            // if (reportData.lastPip) {
+            //     updatesData.flees = `Unsucsessfull flee from ${reportData.lastBeastSeen.name} on ${reportData.distance} with agility ${reportData.lastPip.agility}`;
+            // }
             break;
         case 'deathMessage':
             reportData.isDead = true;
@@ -339,5 +256,5 @@ data.sort((first, second) => {
 });
 
 reportData.receivedItems = _.flatten(reportData.receivedItems);
-console.log(reportData);
-console.log(updatesData); */
+console.log(JSON.stringify(updatesData));
+// console.log(updatesData);
