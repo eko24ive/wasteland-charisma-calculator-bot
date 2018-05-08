584cbbfb34e27af3fab87455d5f7ed7f602936ab
Erik <eko24old@gmail.com>
2018-05-08 13:29:05 +0300

    added beast faced parse

---
+++ src/parsers/parseBeastFaced.js
@@ -0,0 +1,27 @@
+const {
+    regexps
+} = require('../regexp/regexp');
+
+// const parseRegularBeastFaced = data => {
+//     const splitted = data.split('\n');
+
+//     const [, distance] = regexps.campDistanceRegExp.exec(data);
+//     const [, name] = regexps.beastFacedRegExp.exec(data);
+
+//     return {
+//         distance,
+//         name
+//     }
+// };
+
+const parseDungeonBeastFaced = data => {
+    const [, name] = regexps.dungeonBeastFaced.exec(data);
+
+    return {
+        name
+    };
+}
+
+module.exports = {
+    parseDungeonBeastFaced
+};
