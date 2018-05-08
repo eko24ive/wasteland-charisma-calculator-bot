ca03aea9c53b75928fe87554702a7a9b9516cdd3
Erik-Kotsiuba <Erik.Kotsiuba@onseo.biz>
2018-05-08 17:59:05 +0300

    fixed regexp for multiple items received

--- src/regexp/regexp.js
+++ src/regexp/regexp.js
@@ -38,9 +38,13 @@ const beastFacedRegExp = /Во время вылазки на тебя напа�
 const dungeonBeastFacedRegExp = /(.+) перегородил тебе путь./;
 
 const metalAmountRegExp = /(.+) (\d+)/; // 🔗Кубонит 192
-const multipleItemsReceived = /(.+) x(\d+)/; //Провода x1
+const multipleItemsReceived = /(.+) [x|х](\d+)/; //Провода x1
 const emojiRecourceAmount = /(.+) [\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}](\d+)/u; //Кварц 🔹1
 const bonusEmojiResourceAmount = /([\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}])\+(\d+)/u; //Бонус: 🔩+4
+
+const achievmentMessageRegExp = /🏆Достижение получено!/;
+const achievmentContentRegExp = /✅(.+)\n(.+)/;
+
 
 const every = {
     contains: [healthRegExp, hungerRegExp, staminaRegExp, campDistanceRegExp]
