0754b5fada9aa083207156748e71cd393a512c87
Erik <eko24old@gmail.com>
2018-05-25 23:57:13 +0300

    fixes

--- src/utils/processMenu.js
+++ src/utils/processMenu.js
@@ -1,11 +1,4 @@
 const processMenu = menu => {
-    if (menu.content === undefined) {
-        return [{
-            title: 'Назад',
-            name: 'equipment_menu-back'
-        }];
-    }
-
     const buttons = menu.content.map(menuItem => {
         const {title, name} = menuItem;
         
