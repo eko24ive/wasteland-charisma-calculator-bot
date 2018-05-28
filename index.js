e9405fba011ade5f99dfdd429ed901567aada5f1
Erik <eko24old@gmail.com>
2018-05-28 20:59:22 +0300

    buttons revert

--- index.js
+++ index.js
@@ -262,22 +262,6 @@ const defaultKeyboard = bot.keyboard([
     [
         buttons['showGiants'].label,
         buttons['showBeasts'].label,
-        buttons['showDrones'].label
-    ],
-    [
-        buttons['showAllLocations'].label,
-        buttons['showRaidLocations'].label,
-    ],
-    [
-        buttons['hallOfFame'].label,
-        buttons['showHelp'].label
-    ]
-    /*     [
-        buttons['journeyForwardStart'].label
-    ],
-    [
-        buttons['showGiants'].label,
-        buttons['showBeasts'].label,
         buttons['showEquipment'].label,
     ],
     [
@@ -291,7 +275,7 @@ const defaultKeyboard = bot.keyboard([
     ],
     [
         buttons['showHelp'].label
-    ] */
+    ]
 ], {
     resize: true
 });
