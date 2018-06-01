e132a5f66095ae7de667254cb7efde82dc42c335
Erik <eko24old@gmail.com>
2018-06-01 08:07:26 +0300

    created master method to test regexps on string

--- src/utils/matcher.js
+++ src/utils/matcher.js
@@ -1,5 +1,15 @@
 const _ = require('underscore');
 
+const testRegExpsOnString = (regexpSet, string, expectation) => {
+    try {
+        return regexpSet
+            .map(regExp => regExp.test(string))
+            .some(test => test === expectation);
+    } catch (e) {
+        return false;
+    }
+}
+
 const regExpSetMatcher = (string, {
     regexpSet
 }) => {
@@ -9,33 +19,24 @@ const regExpSetMatcher = (string, {
         excludes
     } = regexpSet;
 
+    let containsCheck = true;
     let conditionalCheck = true;
     let excludesCheck = true;
-
 
     contains = _.flatten(contains);
     conditional = _.flatten(conditional);
     excludes = _.flatten(excludes);
 
-
-    const containsCheck = contains
-        .map(regExp => {
-            return regExp.test(string);
-        })
-        .every(test => test === true);
+    if (contains !== undefined && contains.length > 0) {
+        containsCheck = testRegExpsOnString(contains, string, true);
+    }
 
     if (conditional !== undefined && conditional.length > 0) {
-        conditionalCheck = conditional
-            .map(regExp => regExp.test(string))
-            .some(test => test === true);
+        conditionalCheck = testRegExpsOnString(conditional, string, true);
     }
 
     if (excludes !== undefined && excludes.length > 0) {
-        excludesCheck = excludes
-            .map(regExp => {
-                return regExp.test(string);
-            })
-            .every(test => test !== true);
+        excludesCheck = testRegExpsOnString(excludes, string, false);
     }
 
     return (containsCheck && conditionalCheck && excludesCheck);
