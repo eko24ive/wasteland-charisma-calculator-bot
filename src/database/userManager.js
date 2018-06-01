fed1fa5b562cd71e0232f1cdfc5998ef4af5d9d1
Erik.Kotsiuba <Erik.Kotsiuba@onseo.biz>
2018-06-01 16:38:08 +0300

    updated user manager [WIP]

--- src/database/userManager.js
+++ src/database/userManager.js
@@ -1,4 +1,39 @@
 const userManager = User => ({
+    create: ({telegramData, pipData}) => {
+        return new Promise((resolve, reject) => {
+            const newUser = new User({
+
+            });
+
+
+            newUser.save().then(user => {
+                if(user === null) {
+                    resolve(null);
+                }
+
+                resolve(user.toJSON());
+            });
+        });
+    },
+    update: ({id, telegramData, pipData}) => {
+        return new Promise((resolve, reject) => {
+            User.find({'telegram.id': id}).then(user => {
+                // TODO: Detect is pip outdated
+                // TODO: Detect is skills decreased
+                // TODO: Detect is skills upgraded
+                // TODO: Update pip values
+                // TODO: Update pip history
+                // TODO: Detect telegram username change and update if necessary
+
+
+                if(user === null) {
+                    resolve(null);
+                }
+
+                resolve(user.toJSON());
+            });
+        });
+    },
     findByTelegramId: id => {
         return new Promise((resolve, reject) => {
             User.find({'telegram.id': id}).then(user => {
@@ -6,8 +41,8 @@ const userManager = User => ({
                     resolve(null);
                 }
 
-                resolve(user.toJSON);
+                resolve(user.toJSON());
             });
         });
-    }
+    },
 });
