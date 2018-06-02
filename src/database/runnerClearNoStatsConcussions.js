ae6bba3843dc386368f82b951d530d901d5828ba
Erik <eko24old@gmail.com>
2018-06-02 22:34:22 +0300

    added script to fix broken concussions data

---
+++ src/database/runnerClearNoStatsConcussions.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+var async = require('async');
+const _ = require('underscore');
+
+const beastSchema = require('../schemes/beast');
+
+const Beast = mongoose.model('Beast', beastSchema);
+
+const filteredData = {};
+
+mongoose.connect('mongodb://heroku_1q54zt8s:8kbg65u9g98hol9dgithpujahv@ds119490.mlab.com:19490/heroku_1q54zt8s');
+
+let purgeAmount = 0;
+let issuesFound = 0;
+
+Beast.find({
+    'battles.stats': {
+        $exists: false
+    }
+}).then(beasts => {
+    async.forEach(beasts, function (databaseBeast, next) {
+        const beast = databaseBeast.toJSON();
+        const battlesBefore = beast.concussions.length;
+
+        if(_.isEmpty(beast.concussions)) {
+            return next();
+        }
+
+        databaseBeast.concussions.filter(battle => {
+            return _.isEmpty(battle.toJSON().stats);
+        }).forEach(battle => databaseBeast.concussions.remove(battle.id));
+
+        purgeAmount += battlesBefore - databaseBeast.toJSON().concussions.length;
+        issuesFound++;
+
+        databaseBeast.save().then(() => next());
+    }, function (err) {
+        console.log('============================');
+
+        if(issuesFound !== 0 || purgeAmount !== 0) {
+            console.log(`Issues found: ${issuesFound}`);
+            console.log(`Data nodes purged: ${purgeAmount}`);
+        } else {
+            console.log(`✓ All concussion data is intact`)
+        }
+
+        mongoose.disconnect();
+    });
+});
