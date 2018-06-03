9fc2e7648c0984b211fbb375589c431628d8851e
Erik <eko24old@gmail.com>
2018-06-04 00:26:21 +0300

    updated user scheme

--- src/schemes/user.js
+++ src/schemes/user.js
@@ -24,7 +24,7 @@ const userSchema = mongoose.Schema({
         endurance: Number,
         damage: Number,
         armor: Number,
-        timeStamp: Date
+        timeStamp: Number
     },
     points: {
         score: {
