4234a4bc2dfad2923ca2424c50629e5de9d6ffba
Erik <eko24old@gmail.com>
2018-06-03 21:22:44 +0300

    added database specific pip comparsion

---
+++ src/database/utils/comparePips.js
@@ -0,0 +1,25 @@
+const _ = require('underscore');
+
+const blacklistKeys = [
+  'faction',
+  'name',
+  'version'
+]
+
+const comparePips = (newPip, lastPip) => {
+  return Object.keys(lastPip).map(key => {
+    if(!_.contains(blacklistKeys, key)) {
+      const newPipValue = newPip[key];
+      const lastPipValue = lastPip[key];
+
+      if(_.isNumber(newPipValue) && _.isNumber(lastPipValue)) {
+        return (newPipValue - lastPipValue) >= 0;
+      }
+    }
+
+
+    return true;
+  }).every(validation => validation === true);
+}
+
+module.exports = comparePips;
