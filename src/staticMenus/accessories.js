5c4452cdd5bae2a0eeb516b69ff1bf2a1e316932
Katoonok <40026672+Katoonok@users.noreply.github.com>
2018-06-10 21:16:43 +0300

    Equipment menu (#15)
    * Create accessories.js
    
    * Update accessories.js
    
    * Update accessories.js
    
    * Update accessories.js
    
    * Update accessories.js
    
    * Update equipmentMenu.js
    
    * Update accessories.js
    
    * Update equipmentMenu.js
    
    * Update accessories.js
    
    * Update accessories.js
    
    * Update comments
    
    фикс по комментам
    
    * fixes from telegram
    
    new comments
    
    * removed git merge notations
    
    * naming
    
    * naming;
    moved import to top
    
    * added export;formatting;

---
+++ src/staticMenus/accessories.js
@@ -0,0 +1,130 @@
+const _ = require('underscore');
+
+const header = "*Уникальные 🏺Аксессуары*, которые можно получить за заслуги в игре, видимых эффектов не дают:"
+const accessories = [
+  {
+  icon: "🎸",
+  title: "Радиоактивная гитара",
+  comment: false,
+  },{
+  icon: "👓",
+  title: "Очки без линз",
+  comment: false,
+  },{
+  icon: "🎧",
+  title: "Китайские Beats",
+  comment: false,
+  },{
+  icon: "👓",
+  title: "Очки Кларка К.",
+  comment: false,
+  },{
+  icon: "📃",
+  title: "Сертификат долбаеба",
+  comment: false,
+  },{
+  icon: "💠",
+  title: "Немного кристаллов",
+  comment: false,
+  },{
+  icon: "🐞",
+  title:"Божья коровка",
+  comment: false,
+  },{
+  icon: "🗳",
+  title: "Коробка с Мисиксами",
+  comment: false,
+  },{
+  icon: "🔦",
+  title: "Фонарик",
+  comment: false,
+  },{
+  icon: "🍫",
+  title: "Шоколадка без фольги",
+  comment: false,
+  },{
+  icon: "🎂",
+  title: "Торт - это ложь",
+  comment: false,
+  },{
+  icon: "🦆",
+  title: "Просто утка",
+  comment: false,
+  },{
+  icon: "⭕️",
+  title: "Покебол",
+  comment: false,
+  },{
+  icon: "🍷",
+  title: "Винишко из мутафруктов",
+  comment: false,
+  },{
+  icon: "🎟",
+  title: "Талончик на нихуя",
+  comment: false,
+  },{
+  icon: "🚀",
+  title: "Межпланетный экспресс",
+  comment: false,
+  },{
+  icon: "🍀",
+  title: "Клевер-мутант",
+  comment: false,
+  },{
+  icon: "💧",
+  title: "Слеза котосвина",
+  comment: false,
+  },{
+  icon: "🌐",
+  title: "ShitCoin",
+  comment: false,
+  },{
+  icon: "⚖️",
+  title: "Право на бунд",
+  comment: false,
+  },{
+  icon: "🐼",
+  title: "Карманная панда",
+  comment: false,
+  },{
+  icon: "🖋",
+  title: "Перо Ванаминго",
+  comment: "Выдано в единичном экземпляре лучшему охотнику на Ванаминго",
+  },{
+  icon: "🍄",
+  title: "Вдохновение стартапов",
+  comment: false,
+  },{
+  icon: "🥄",
+  title: "There is no spoon",
+  comment: false,
+  },{
+  icon: "👂",
+  title: "Качественное ухо",
+  comment: false,
+  },{
+  icon: "🌝",
+  title: "Лицо альбиноса",
+  comment: false,
+  },{
+  icon: "📖",
+  title: "ММОРПГ для чайников",
+  comment: false,
+  },{
+  icon: "👻",
+  title: "Каспер, мертвый подросток",
+  comment: false,
+  }
+]
+
+const uniqueAccessories = _.sortBy(accessories, accessory => accessory.title).map(({
+  icon,
+  title,
+  comment
+}) => {
+  return `${icon} *${title}* ${comment ? `(${comment})` : ''}`;
+}).join('\n');
+
+const uniqueAccessoriesText = `${header}\n${uniqueAccessories}`;
+
+module.exports = uniqueAccessories;
