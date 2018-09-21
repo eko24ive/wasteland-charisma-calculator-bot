2d6e9222916aca929d77298196aabbf8554a364b
Erik Kotsiuba <eko24old@gmail.com>
2018-09-22 01:06:45 +0300

    fixed typo

--- src/utils/validateForwardDate.js
+++ src/utils/validateForwardDate.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '../../.env' });
 
 const validateForwardDate = (date) => {
-  if (date < 1537304400 && process.env.ENV === 'PRDUCTION') {
+  if (date < 1537304400 && process.env.ENV === 'PRODUCTION') {
     return false;
   }
 
