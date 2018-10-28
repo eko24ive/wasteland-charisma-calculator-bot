820198e0509e95525f9204757a6fdd75d9dc7262
Erik Kotsiuba <eko24old@gmail.com>
2018-10-28 14:07:54 +0300

    Time cost for skill-o-matic (#132)
    * added jsonfile dep
    
    * updated skill-o-matic feature
    
    * fixed total discount infop
    
    * removed "equal" from comparssion
    
    * eol

--- src/calculateUpgrade.js
+++ src/calculateUpgrade.js
@@ -4,7 +4,8 @@ const constants = require('./constants/constants');
 const defaultSkillCost = require('./constants/defaultSkillCost');
 const defaultCharismaCost = require('./constants/defaultCharismaCost');
 const mobs = require('./constants/mobs');
-const mobsRanges = require('./constants/mobsRanges');
+const ranges = require('./utils/getRanges');
+const timeToTravel = require('./utils/timeToTravel');
 
 const skillMap = {
   '💪 Сила': 'strength',
@@ -108,9 +109,6 @@ const calculateAmountOfRaids = (
   skillRangeTo,
   upgradeSkill,
 ) => {
-  const distanceOfRanges = {};
-  const mobsFillment = [];
-
   const totalSpend = amountToSpend(
     upgradeSkill,
     charismaLevel,
@@ -122,30 +120,18 @@ const calculateAmountOfRaids = (
     return null;
   }
 
-  const scopeOfRanges = mobsRanges.filter((range) => {
-    const [start, end] = range.split('-');
+  const availableMobsOnDistance = mobs.filter((beast) => {
+    const { kmMin, kmMax } = beast;
 
-    return Number(end) <= Number(reachableDistance) || Number(reachableDistance) >= Number(start);
+    return Number(kmMin) < Number(reachableDistance) || Number(reachableDistance) > Number(kmMax);
   });
 
-  scopeOfRanges.forEach((range) => {
-    const [start, end] = range.split('-');
+  const mobsFillment = ranges.ranges.map((range) => {
+    const [min, max] = range;
 
-    distanceOfRanges[range] = end - start;
-  });
+    const mobsOnRange = availableMobsOnDistance.filter(({ kmMin, kmMax }) => Number(kmMin) <= Number(max) || Number(min) >= Number(kmMax));
 
-  const getRandomItem = array => array[Math.floor(Math.random() * array.length)];
-
-  scopeOfRanges.forEach((range) => {
-    const distanceOfRange = distanceOfRanges[range];
-    const amountOfIterations = distanceOfRange < 2 ? distanceOfRange : Math.floor(Math.random() * 2) + 1;
-    const mobsForRange = mobs[range];
-
-    // FIXME: Might be cause of skillupgrade issue;
-    for (let i = amountOfIterations; i > 0; i -= 1) {
-      const item = getRandomItem(mobsForRange);
-      mobsFillment.push(item);
-    }
+    return mobsOnRange[Math.floor(Math.random() * mobsOnRange.length)];
   });
 
   const bestCaseScenario = {
@@ -226,7 +212,17 @@ const calculateUpgrade = ({
   const reachableDistance = Number(/\d*/.exec(reachableKm).pop());
 
   const calculations = {
-    amountOfSavedFunds: calculatePerkDiscount(charismaLevel),
+    amountOfSavedFunds: amountToSpend(
+      upgradeSkill,
+      1,
+      currentSkillLevel,
+      upgradeTo,
+    ) - amountToSpend(
+      upgradeSkill,
+      charismaLevel,
+      currentSkillLevel,
+      upgradeTo,
+    ),
     amountToSpend: amountToSpend(
       upgradeSkill,
       charismaLevel,
@@ -244,6 +240,12 @@ const calculateUpgrade = ({
   };
 
   const spentOnSkill = calculateSpentOnSkill(currentSkillLevel);
+
+  const raidsAmount = Math.floor(calculations.raidsInfo.worstCaseScenario.amountOfRaids);
+  const timeToFarm = Math.floor((timeToTravel(pip.endurance, reachableDistance) * raidsAmount) / 60 / 60);
+
+  const displayTimeToFarm = timeToFarm === 0 ? (timeToTravel(pip.endurance, reachableDistance) * raidsAmount).toFixed(2) : timeToFarm;
+
 
   /*
     При самом удачном стечении обсоятельств тебе необходимо сделать примерно ${Math.ceil(calculations.raidsInfo.bestCaseScenario.amountOfRaids)} 👣 ходок:
@@ -263,7 +265,11 @@ _Всего ты потратил ${formatNubmer(spentOnSkill)} 🕳 крыше�
 Необходимо потратить ${formatNubmer(calculations.amountToSpend)} 🕳 крышек для прокачки навыка \`${upgradeSkill}\` от ${currentSkillLevel} уровня до ${upgradeTo} уровня
 
 
-Тебе необходимо сделать примерно *${Math.floor((calculations.raidsInfo.worstCaseScenario.amountOfRaids + 2) * 1.5)} 👣 ходок*:
+Тебе необходимо сделать примерно *${raidsAmount || '<1'} 👣 ходок*.
+
+${raidsAmount > 0 ? `C твоей 🔋Выносливостью на это потребуётся примерно ${displayTimeToFarm} часов.` : ''}
+_Без учёта рейдов, игровых событий, лагов, солнечных затмений и прочей хуйни_
+
 \`Из-за недавнего обновления Wasteland Wars данные для расчёта ходок работают в эксперементальном режиме\`
 `;
   /*
