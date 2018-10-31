c554d149cb1472a7b687b45b29bb3124eb7210f9
Erik Kotsiuba <eko24old@gmail.com>
2018-11-01 00:41:48 +0200

    logging

--- src/views/routedBeastView.js
+++ src/views/routedBeastView.js
@@ -29,6 +29,10 @@ const routedBeastView = (Beast, seachParams, route = null, config) => new Promis
 
   Beast.findOne(seachParams).then((fBeast) => {
     if (fBeast !== null) {
+      console.log('==================');
+      console.log(`Browsing: ${fBeast._id.toJSON()} `);
+      console.log('==================');
+
       let isRangeDeprecated = INFO_ABSENT;
       let isLootDeprecated = INFO_ABSENT;
       const isBattlesDeprecated = {
