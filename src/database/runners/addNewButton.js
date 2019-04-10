95d03c40ac463913f55f40b6b37b2b5692d434da
eko24ive <eko24old@gmail.com>
2019-04-10 18:20:06 +0300

    runners

---
+++ src/database/runners/addNewButton.js
@@ -0,0 +1,39 @@
+require('dotenv').config({ path: '../../.env' });
+const mongoose = require('mongoose');
+const async = require('async');
+const _ = require('underscore');
+
+const userSchema = require('../../schemes/user');
+
+const User = mongoose.model('User', userSchema);
+
+mongoose.connect(process.env.RUNNER_MONGO_DB);
+
+let total = 0;
+let processed = 0;
+
+console.log('===START===');
+
+User.find().then((users) => {
+  total = users.length;
+
+  async.forEach(users, (user, next) => {
+    user.settings.buttons.push({
+      index: 19,
+      label: '📊 Статистика',
+      command: '/myforwardstats',
+      state: true,
+      order: 7.6,
+    });
+
+    user.save().then(() => {
+      processed += 1;
+      console.log(`${processed}/${total}`);
+
+      next();
+    });
+  }, () => {
+    mongoose.disconnect();
+    console.log('===END===');
+  });
+});
---
+++ src/database/runners/userForwards.js
@@ -0,0 +1,34 @@
+require('dotenv').config({ path: '../../.env' });
+const mongoose = require('mongoose');
+const async = require('async');
+const _ = require('underscore');
+
+const userSchema = require('../../schemes/user');
+const userDefaults = require('../../schemes/defaults/user');
+
+const User = mongoose.model('User', userSchema);
+
+mongoose.connect(process.env.RUNNER_MONGO_DB);
+
+let total = 0;
+let processed = 0;
+
+console.log('===START===');
+
+User.find().then((users) => {
+  total = users.length;
+
+  async.forEach(users, (user, next) => {
+    user.points.forwards = userDefaults.points.forwards;
+
+    user.save().then(() => {
+      processed += 1;
+      console.log(`${processed}/${total}`);
+
+      next();
+    });
+  }, () => {
+    mongoose.disconnect();
+    console.log('===END===');
+  });
+});
